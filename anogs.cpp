2b9c1dc92886d8499062b0ab40f45ca6faf08252
gokurohit <156572119+gokurohit@users.noreply.github.com>
2024-02-15 19:31:42 +0530

    Add files via upload

---
+++ anogs.cpp
@@ -0,0 +1,6 @@
+MemoryPatch::createWithHex("libanogs.so",0x11691A14,"h 00 00 00 00 C7 AD 00 00 48 22 00 D0").Modify();
+MemoryPatch::createWithHex("libanogs.so",0x116919D4,"h 00 00 00 00 C8 05 00 00 48 22 00 D0").Modify(); 
+MemoryPatch::createWithHex("libanogs.so",0x1365D0,"h 00 00 00 00 98 00 00 00 48 22 00 D0").Modify(); 
+MemoryPatch::createWithHex("libanogs.so",0x11692AC4,"h 00 00 00 00 92 D8 01 00 48 22 00 D0").Modify(); 
+MemoryPatch::createWithHex("libanogs.so",0x11692A64,"h 00 00 00 00 D9 0E 00 00 48 22 00 D0").Modify(); 
+MemoryPatch::createWithHex("libanogs.so",0x11692A4C,"h 00 00 00 00 06 BD 00 00 48 22 00 D0").Modify(); 
